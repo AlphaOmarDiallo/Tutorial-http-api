60bf602dd626f32312282c69eb986ee4ec176b8a
alpha <alphaomar.diallo@gmail.com>
2022-12-26 08:40:21 +0200

    All routes set up. Ready to test

--- src/main/kotlin/com/alphaomardiallo/plugins/Routing.kt
+++ src/main/kotlin/com/alphaomardiallo/plugins/Routing.kt
@@ -1,11 +1,17 @@
 package com.alphaomardiallo.plugins
 
 import com.alphaomardiallo.routes.customerRouting
+import com.alphaomardiallo.routes.getOrderRoute
+import com.alphaomardiallo.routes.listOrdersRoute
+import com.alphaomardiallo.routes.totalizeOrderRoute
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         customerRouting()
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
     }
 }
